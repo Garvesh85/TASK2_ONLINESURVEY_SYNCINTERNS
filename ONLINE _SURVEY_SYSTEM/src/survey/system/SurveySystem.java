d6e964123ba60081b11aa4d69d0eb09c51a93f65
Garvesh Dhiman <96383652+Garvesh85@users.noreply.github.com>
2023-07-11 14:49:03 +0530

    Add files via upload

---
+++ ONLINE _SURVEY_SYSTEM/src/survey/system/SurveySystem.java
@@ -0,0 +1,19 @@
+
+package survey.system;
+
+/**
+ * @author GARVESH
+ */
+
+public class SurveySystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //setVisible(false);
+        new register().setVisible(true);
+    }
+
+}
